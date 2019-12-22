75dd80298877f7231744d87e8189f59cb4349014
VaN121314 <59138779+VaN121314@users.noreply.github.com>
2019-12-22 17:21:28 +0500

    Add files via upload

---
+++ Menu.js
@@ -0,0 +1,8 @@
+document.writeln("<ul class="topbar">");
+document.writeln("<li><A HREF=Ultramarine.htm>Ультрамарины</A></li>");
+document.writeln("<li><A HREF=ImpK.htm>Имперские Кулаки</A></li>");
+document.writeln("<li><A HREF=KV.htm>Космические Волки</A></li>");
+document.writeln("<li><A HREF=Salamandri.htm>Саламандры</A></li>");
+document.writeln("<li><A HREF=DA.htm>Тёмные Ангелы</A></li>");
+document.writeln("</ul>");
+document.writeln("qdqwdqwdqwdqwdqw");
